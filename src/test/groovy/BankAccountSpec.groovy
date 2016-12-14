125c1bf03d5efee494eba89b8b4d286b2359e05f
Alin Pandichi <alin.pandichi@mozaicworks.com>
2016-12-14 15:27:35 +0200

    Clarified the given inputs and the action performed

--- src/test/groovy/BankAccountSpec.groovy
+++ src/test/groovy/BankAccountSpec.groovy
@@ -3,9 +3,14 @@ import spock.lang.Specification
 
 class BankAccountSpec extends Specification {
 	def "after a 1000 deposit in a balance with value 0, the balance has the value 1000"(){
-		when:
+		given:
+		def initialBalance = 0
+		def theDepositValue = 1000
 		def theExpectedBalanceValue = 1000
-		def theActualBalanceValue = 0 + 1000
+
+		when:
+		def doTheDeposit = initialBalance + theDepositValue
+		def theActualBalanceValue = doTheDeposit
 
 		then:
 		theExpectedBalanceValue == theActualBalanceValue
